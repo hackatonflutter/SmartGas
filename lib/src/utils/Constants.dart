792b457fe24c38c1388f8f6e32fee19b1c26cd6c
Edu David Moreno Ccama <edu.david2706@gmail.com>
2019-06-01 16:29:20 -0500

    commit

--- lib/src/utils/Constants.dart
+++ lib/src/utils/Constants.dart
@@ -1,13 +1,13 @@
 import 'package:amazon_cognito_identity_dart/cognito.dart';
 
 class Constants {
-static const awsUserPoolId = 'us-east-1_dfUNX6DPn';
-static const awsClientId = '2lbeh8s6228f1gh7j26vhmt1nm';
+static const awsUserPoolId = '';
+static const awsClientId = '';
 
-static const identityPoolId = 'us-east-1:212026313232:userpool/us-east-1_dfUNX6DPn';
+static const identityPoolId = '';
 
 // Setup endpoints here:
-static const region = 'us-east-1';
+static const region = '';
 //static const endpoint = 'https://xxxxxxxxxx.execute-api.ap-southeast-1.amazonaws.com/dev';
 
 static final userPool = new CognitoUserPool(awsUserPoolId, awsClientId);
