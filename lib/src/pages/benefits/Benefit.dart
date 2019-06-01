20b80b2aca1fc73f52252830a44a1dec9589ac77
Jonathan Choy <jchoy8890@gmail.com>
2019-06-01 16:08:31 -0500

    Se agrega view

--- lib/src/pages/benefits/Benefit.dart
+++ lib/src/pages/benefits/Benefit.dart
@@ -2,24 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(Benefits());
 
-
-      //titlePercent: 'Tu porcentaje de consumo es: ',
+//titlePercent: 'Tu porcentaje de consumo es: ',
 
 class Benefits extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: BenefitPage(title: 'Flutter Demo Home Page'),
-    );
+    return new MaterialApp(
+        title: 'Benefits',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new BenefitPage(title: 'Benefits'));
   }
-  
-  }
+}
 
-  class BenefitPage extends StatefulWidget {
+class BenefitPage extends StatefulWidget {
   BenefitPage({Key key, this.title}) : super(key: key);
   final String title;
 
@@ -28,39 +25,90 @@ class Benefits extends StatelessWidget {
 }
 
 class _BenefitPageState extends State<BenefitPage> {
+  List _selectedBenefits = List();
 
-  int _counter = 0;
+  Map<String, dynamic> _benefits = {
+    "responseCode": "1",
+    "responseText": "Elija su beneficio",
+    "responseBody": [
+      {"benefit_id": "1", "benefit_name": "Lavado de auto"},
+      {"benefit_id": "2", "benefit_name": "Carga de bateria"},
+      {"benefit_id": "3", "benefit_name": "Pintado de auto"}
+    ],
+    "responseTotalResult":
+        3 // Total result is 3 here becasue we have 3 categories in responseBody.
+  };
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-    @override
+  @override
   Widget build(BuildContext context) {
-     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+    final Size screenSize = MediaQuery.of(context).size;
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Benefits'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: new Builder(
+        builder: (BuildContext context) {
+          return new Container(
+            child: new Form(
+              key: _formKey,
+              child: new ListView(
+                children: <Widget>[
+                  new Container(
+                      padding: new EdgeInsets.all(20.0),
+                      width: screenSize.width,
+                      child: new Text('Tu porcentaje de consumo:')),
+                  new Container(
+                      padding: new EdgeInsets.all(20.0),
+                      width: screenSize.width,
+                      child: new Text('.....................................')),
+                  new ListView.builder(
+                      itemCount: _benefits['responseTotalResult'],
+                      itemBuilder: (BuildContext context, int index) {
+                        new CheckboxListTile(
+                            value: _selectedBenefits.contains(
+                                _benefits['responseBody'][index]['benefit_id']),
+                            onChanged: (bool selected) {
+                              _onBenefitSelected(
+                                  selected,
+                                  _benefits['responseBody'][index]
+                                      ['benefit_id']);
+                            },
+                            title: Text(_benefits['responseBody'][index]
+                                ['benefit_id']));
+                      }),
+                  new Container(
+                    child: new RaisedButton(
+                      shape: StadiumBorder(),
+                      child: new Text(
+                        'Aceptar',
+                        style: new TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        //_submit(context);
+                      },
+                      color: Colors.purple,
+                    ),
+                  )
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+          );
+        },
       ),
     );
+  }
+
+  void _onBenefitSelected(bool selected, benefit_id) {
+    if (selected == true) {
+      setState(() {
+        _selectedBenefits.add(benefit_id);
+      });
+    } else {
+      setState(() {
+        _selectedBenefits.remove(benefit_id);
+      });
+    }
   }
 }
