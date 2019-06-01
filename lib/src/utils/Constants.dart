287dacc7b4d22d3025f81ef50d499739ed25ebf0
Renato Mercado <renatomercadoluna@hotmail.com>
2019-06-01 14:00:39 -0500

    commit login ready

--- lib/src/utils/Constants.dart
+++ lib/src/utils/Constants.dart
@@ -1,4 +1,19 @@
 import 'package:amazon_cognito_identity_dart/cognito.dart';
 
 class Constants{
+
+// Setup AWS User Pool Id & Client Id settings here:
+
+static const awsUserPoolId = "";
+static const awsClientId = "";
+
+static const identityPoolId = "";
+
+// Setup endpoints here:
+static const region = 'us-east-1';
+//static const endpoint = 'https://xxxxxxxxxx.execute-api.ap-southeast-1.amazonaws.com/dev';
+
+static final userPool = new CognitoUserPool(awsUserPoolId, awsClientId);
+
+
 }
